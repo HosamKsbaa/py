4f01a91bd0b24d81bffbee6f35e46c0a664baf59
HosamKsbaa <35602111+HosamKsbaa@users.noreply.github.com>
2022-07-28 16:27:25 +0200

    7a7a

--- MyFirstProject/base/urls.py
+++ MyFirstProject/base/urls.py
@@ -8,4 +8,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('room/', views.room, name='room'),
+
 ]
--- MyFirstProject/base/views.py
+++ MyFirstProject/base/views.py
@@ -1,11 +1,20 @@
+from multiprocessing import context
+from unicodedata import name
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
+
+
+
+room =[
+    {'id':1,"name":"room1"}, {'id':1,"name":"room1"}, {'id':1,"name":"room1"}, {'id':1,"name":"room1"}
+]
 def home(request):
-    return render(request, 'home.html')
+    context = {'room': room}
+    return render(request, 'base/home.html',context)
 
 
 def room(request):
-    return render(request, 'home.html')
+    return render(request, 'base/room.html')
