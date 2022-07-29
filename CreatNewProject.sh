9654440712edf75a6092f52bef4f1031703afbdc
HosamKsbaa <35602111+HosamKsbaa@users.noreply.github.com>
2022-07-29 15:55:06 +0200

    help plz

--- CreatNewProject.sh
+++ CreatNewProject.sh
@@ -1,7 +1,7 @@
 pip install virtualenv
 virtualenv -p python3 env
 Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
-env/scripts/activate
+MyFirstProject/env/scripts/activate
 pip install django
 django-admin 
 django-admin startproject MyFirstProject 
--- MyFirstProject/base/views.py
+++ MyFirstProject/base/views.py
@@ -9,7 +9,7 @@ from django.shortcuts import render
 
 
 room =[
-    {'id':1,"name":"room1"}, {'id':1,"name":"room1"}, {'id':1,"name":"room1"}, {'id':1,"name":"room1"}
+    {'id':1,"name":"room1"}, {'id':2,"name":"room2"}, {'id':3,"name":"room3"}, {'id':4,"name":"room4"}
 ]
 def home(request):
     context = {'room': room}
